3d6d1d741097c1692ef649b924650ece1967889c
Luka Nedimović <lukazen123@gmail.com>
2025-08-22 17:44:22 +0200

    chore: Add setup script and general conda env

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# This script will set up the Conda environment and install the dependencies
+
+export PROJECT_NAME="osteo_gdl"
+CONDA_ENV_NAME="${PROJECT_NAME}_venv"
+PYTHON_VERSION="3.9"
+
+# Check if the conda environment exists
+if conda env list | grep -q "$CONDA_ENV_NAME"; then
+    echo "Conda environment '$CONDA_ENV_NAME' already exists. Updating it..."
+    conda env update --name "$CONDA_ENV_NAME" --file environment.yaml --prune
+else
+    echo "Conda environment '$CONDA_ENV_NAME' not found. Creating it from environment.yaml..."
+    conda env create --name "$CONDA_ENV_NAME" --file environment.yaml
+fi
+
+# Activate the environment
+conda activate "$CONDA_ENV_NAME"
+
+echo "Environment setup complete."
+
+# General environment variables
+export PROJ_ROOT=$PWD
+export PYTHONPATH=${PROJ_ROOT}
